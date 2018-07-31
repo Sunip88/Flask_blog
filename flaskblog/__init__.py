f22e6b600624074f8632a44fc3148e2f81fb726a
Sunip88 <drzep4@gmail.com>
2018-07-31 15:17:47 +0200

    added logout, account

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0956216755cc68d17ee4ee8862451d93'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+loginmenager = LoginManager(app)
+loginmenager.login_view = 'login'
+loginmenager.login_message_category = 'info'
 
 from flaskblog import routes
